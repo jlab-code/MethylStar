bd4490bbb1b41fcb4a731bcc587a295c6c69475b
Yadi <shahryary@gmail.com>
2019-12-18 14:08:25 +0100

    performance improved

--- src/bash/detect.sh
+++ src/bash/detect.sh
@@ -8,7 +8,7 @@ npar=$(cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l)
 if [ "$3" = "0" ];
 then
 
-                if [ "$1" = "Arabidopsis" ]; 
+                if [ "$1" = "Arabidopsis" ] || [ "$1" = "Rice" ]; 
                 then
 
                                         if [ "$2" = "trimm" ]; 
@@ -55,7 +55,7 @@ then
                                         fi
 
 
-                elif [ "$1" = "Maize" ];
+                elif [ "$1" = "Maize" ] || [ "$1" = "Human" ] || [ "$1" = "scBS-Seq" ] ;
                 then 
 
                                         if [ "$2" = "trimm" ]; 
@@ -100,15 +100,16 @@ then
                                                 sed -i "s/bis_parallel=.*/bis_parallel=$bis_parallel/g" $curr_dir/tmp.conf
                                                 sed -i "s/npar=.*/npar=$npar/g" $curr_dir/tmp.conf
                                         fi
-                elif [ "$1" = "Human" ];
+
+                elif [ "$1" = "Others" ];
                 then 
                                         if [ "$2" = "trimm" ]; 
                                         then
                                                 n_th=1
-                                                if [ $npar -gt 88 ]; then npar=5 && n_th=5;
-                                                        elif [ $npar -gt 64 -a $npar -le 88 ]; then npar=5 && n_th=4;
-                                                        elif [ $npar -gt 32 -a $npar -le 64 ]; then npar=3 && n_th=4;
-                                                        elif [ $npar -gt 16 -a $npar -le 32 ]; then npar=2 && n_th=4;
+                                                if [ $npar -gt 88 ]; then npar=4 && n_th=4;
+                                                        elif [ $npar -gt 64 -a $npar -le 88 ]; then npar=4 && n_th=4;
+                                                        elif [ $npar -gt 32 -a $npar -le 64 ]; then npar=3 && n_th=3;
+                                                        elif [ $npar -gt 16 -a $npar -le 32 ]; then npar=2 && n_th=2;
                                                         elif [ $npar -gt 4 -a $npar -le 16 ]; then  npar=1;
                                                 fi
                                                 sed -i "s/n_th=.*/n_th=$n_th/g" $curr_dir/tmp.conf
@@ -121,53 +122,8 @@ then
                                         then
                                                 Nthreads=2
                                                 bis_parallel=2
-                                                if [ $npar -gt 88 ]; then npar=3 && bis_parallel=8 && Nthreads=5;
-                                                        elif [ $npar -gt 64 -a $npar -le 88 ]; then npar=2 && bis_parallel=6 && Nthreads=4;
-                                                        elif [ $npar -gt 32 -a $npar -le 64 ]; then npar=1 && bis_parallel=4 && Nthreads=3;
-                                                        elif [ $npar -gt 16 -a $npar -le 32 ]; then npar=1 && bis_parallel=3 && Nthreads=2;
-                                                        elif [ $npar -gt 4 -a $npar -le 16 ]; then  npar=1 && sed -i "s/parallel_mode=.*/parallel_mode=false/g" config/pipeline.conf;
-                                                fi
-                                                sed -i "s/bis_parallel=.*/bis_parallel=$bis_parallel/g" $curr_dir/tmp.conf
-                                                sed -i "s/npar=.*/npar=$npar/g" $curr_dir/tmp.conf
-                                                sed -i "s/Nthreads=.*/Nthreads=$Nthreads/g" $curr_dir/tmp.conf
-                                        fi
-
-                                        if [ "$2" = "bismeth" ]; 
-                                        then
-                                                bis_parallel=6
-                                                if [ $npar -gt 88 ]; then npar=4 && bis_parallel=8;
-                                                        elif [ $npar -gt 64 -a $npar -le 88 ]; then npar=2 && bis_parallel=6;
-                                                        elif [ $npar -gt 32 -a $npar -le 64 ]; then npar=1 && bis_parallel=5;
-                                                        elif [ $npar -gt 16 -a $npar -le 32 ]; then npar=1 && bis_parallel=4;
-                                                        elif [ $npar -gt 4 -a $npar -le 16 ]; then  npar=1 && sed -i "s/parallel_mode=.*/parallel_mode=false/g" config/pipeline.conf ;
-                                                fi
-                                                sed -i "s/bis_parallel=.*/bis_parallel=$bis_parallel/g" $curr_dir/tmp.conf
-                                                sed -i "s/npar=.*/npar=$npar/g" $curr_dir/tmp.conf
-                                        fi
-
-                elif [ "$1" = "scBS-Seq" ];
-                then 
-                                        if [ "$2" = "trimm" ]; 
-                                        then
-                                                n_th=1
-                                                if [ $npar -gt 88 ]; then npar=5 && n_th=5;
-                                                        elif [ $npar -gt 64 -a $npar -le 88 ]; then npar=5 && n_th=4;
-                                                        elif [ $npar -gt 32 -a $npar -le 64 ]; then npar=3 && n_th=4;
-                                                        elif [ $npar -gt 16 -a $npar -le 32 ]; then npar=2 && n_th=4;
-                                                        elif [ $npar -gt 4 -a $npar -le 16 ]; then  npar=1;
-                                                fi
-                                                sed -i "s/n_th=.*/n_th=$n_th/g" $curr_dir/tmp.conf
-                                                sed -i "s/npar=.*/npar=$npar/g" $curr_dir/tmp.conf
-                                        fi
-                                        #---------------------
-                                        # Bismark mapper 
-                                        #---------------------
-                                        if [ "$2" = "bismap" ]; 
-                                        then
-                                                Nthreads=2
-                                                bis_parallel=2
-                                                if [ $npar -gt 88 ]; then npar=3 && bis_parallel=8 && Nthreads=5;
-                                                        elif [ $npar -gt 64 -a $npar -le 88 ]; then npar=2 && bis_parallel=6 && Nthreads=4;
+                                                if [ $npar -gt 88 ]; then npar=3 && bis_parallel=6 && Nthreads=4;
+                                                        elif [ $npar -gt 64 -a $npar -le 88 ]; then npar=2 && bis_parallel=5 && Nthreads=4;
                                                         elif [ $npar -gt 32 -a $npar -le 64 ]; then npar=1 && bis_parallel=4 && Nthreads=3;
                                                         elif [ $npar -gt 16 -a $npar -le 32 ]; then npar=1 && bis_parallel=3 && Nthreads=2;
                                                         elif [ $npar -gt 4 -a $npar -le 16 ]; then  npar=1 && sed -i "s/parallel_mode=.*/parallel_mode=false/g" config/pipeline.conf;
